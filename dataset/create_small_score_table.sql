0cb98ae8a173afe12a921e23a2897be26be970aa
ypc82 <ypc82@hotmail.com>
2020-04-20 16:18:14 -0400

    add score table (#3)
    * generate small tables for testing
    
    * add create_score_table.sql
    
    Co-authored-by: ypc <ypc82@iis.sinica.edu.tw>

---
+++ dataset/create_small_score_table.sql
@@ -0,0 +1,26 @@
+-- This code is the same as 'create_score_table.sql', 
+-- except based on the small testing tables.
+
+DROP TABLE IF EXISTS s_paper_score cascade;
+
+CREATE TABLE s_paper_score AS (
+    with author_score(author_id, author_s) as (
+        select authid, count(distinct paperid)
+        from s_paperauths
+        group by authid
+    ),
+    paper_score_temp(paper_id, paper_s) as (
+        select pa.paperid, sum(a_s.author_s)
+        from author_score as a_s, s_paperauths as pa
+        where pa.authid = a_s.author_id
+        group by pa.paperid
+    )
+
+    select p_s.paper_s as paper_score, p.id as paper_id, p.name as paper_name, 
+        v.id as venue_id, v.year as venue_year, v.type as venue_type
+    from paper_score_temp as p_s, s_papers as p, s_venue as v
+    where p_s.paper_id = p.id and p.venue = v.id
+);
+
+
+
