56dd5f87bd48e263ea2ef88bb1a0dcb293668c0f
ypc82 <ypc82@hotmail.com>
2020-04-20 16:14:45 -0400

    add create_score_table.sql

---
+++ dataset/create_score_table.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS paper_score cascade;
+
+CREATE TABLE paper_score AS (
+    with author_score(author_id, author_s) as (
+        select authid, count(distinct paperid)
+        from paperauths
+        group by authid
+    ),
+    paper_score_temp(paper_id, paper_s) as (
+        select pa.paperid, sum(a_s.author_s)
+        from author_score as a_s, paperauths as pa
+        where pa.authid = a_s.author_id
+        group by pa.paperid
+    )
+
+    select p_s.paper_s as paper_score, p.id as paper_id, p.name as paper_name, 
+        v.id as venue_id, v.year as venue_year, v.type as venue_type
+    from paper_score_temp as p_s, papers as p, venue as v
+    where p_s.paper_id = p.id and p.venue = v.id
+);
+
+
