f4320df87ff46c30f5c20ac94c91587c950916f9
Mark Yen <mark11yan@gmail.com>
2020-04-24 19:20:17 -0700

    debug: need sum for group by

--- dataset/create_score_table.sql
+++ dataset/create_score_table.sql
@@ -13,10 +13,11 @@ CREATE TABLE paper_score AS (
         group by pa.paperid
     )
 
-    select p_s.paper_s as paper_score, 
+    select sum(p_s.paper_s) as paper_score,
         v.name as venue_name, v.year as venue_year, v.type as venue_type
     from paper_score_temp as p_s, papers as p, venue as v
     where p_s.paper_id = p.id and p.venue = v.id
+    group by venue_name, venue_year, venue_type
 );
 
 alter table paper_score add primary key (venue_name, venue_year, venue_type);
--- dataset/create_small_score_table.sql
+++ dataset/create_small_score_table.sql
@@ -16,10 +16,11 @@ CREATE TABLE s_paper_score AS (
         group by pa.paperid
     )
 
-    select p_s.paper_s as paper_score, 
+    select sum(p_s.paper_s) as paper_score,
         v.name as venue_name, v.year as venue_year, v.type as venue_type
     from paper_score_temp as p_s, s_papers as p, s_venue as v
     where p_s.paper_id = p.id and p.venue = v.id
+    group by venue_name, venue_year, venue_type
 );
 
 alter table s_paper_score add primary key (venue_name, venue_year, venue_type);
